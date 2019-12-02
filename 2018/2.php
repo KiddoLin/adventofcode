f9459f18a7bb929d031c62a306e1f4e001678c63
Kiddo <jiaqun.l@mofadeng.com>
2019-12-02 10:47:57 +0800

    2018/2.php

---
+++ 2018/2.php
@@ -0,0 +1,316 @@
+<?php
+    $str = 'ybruvapdgixszyckwtfqjonsie
+mbruvapxghslyyckwtfqjonsie
+mbruvapdghslzyckwtkujonsie
+rwruvapdghxlzyckwtfqjcnsie
+obruvapdgtxlzyckwtfqionsie
+lbruvapdghxqzyckwtfqjfnsie
+mbrunapdghxlzyccatfqjonsie
+mbruvapdghxlzyokltfqjdnsie
+ybruvapdghxlzmckwtfqjmnsie
+mbruwaadghxdzyckwtfqjonsie
+muruvapdghxlzyckvtfqjonsim
+mbruvapdghxlkyckwtxqjonjie
+mbruvaqdghxlzyckwtfqjrnnie
+mwruvapdghdlzyckttfqjonsie
+mbruvapdgtelzyckwxfqjonsie
+mbruvapdohxlzvckwtfqjonhie
+mbrugapdgbxlzyckwtfqjynsie
+mbruvapdghxlzyckwtlqjonjiu
+mbruvapwghxlzyckwafqjonbie
+wbruvapdghxlhyckwtfqjonsii
+mbruvapdghxlzyckwtcqnonsiq
+mbyuvapighxlzybkwtfqjonsie
+mbrrvapdghxvzyckwtfqjonsio
+mhruvapdghrlzyckwtfzjonsie
+mtruvapvghxlzyckwtfnjonsie
+mmrlhapdghxlzyckwtfqjonsie
+mbruvapdgpxlzyjkwtfqjovsie
+mbrucapdghxlzymkwtzqjonsie
+mbeuvafdghxlzyckwtfqjonwie
+mbruvapcghxlayckwtfqjonsii
+mbruvabdghxlzyckwtfqyansie
+mbruvjpdghxlzyckwtfqgfnsie
+lbruvapdghxlzyckwtfqjonriv
+mbrupapdghxlzycjwtfqronsie
+mbpuvapdthxlzymkwtfqjonsie
+mbiuvapdgixlzyckwxfqjonsie
+mbruvapdghxyzyckwtfcjonsbe
+mbrurapkghxlzyckwtfqjonzie
+mbrufapdrhxlzyciwtfqjonsie
+mbruvapdghxlzbckwtfqjoisae
+ubruhapdghxlzuckwtfqjonsie
+mbruvapdjhulzyckwtfqjonshe
+mbruwapdgyxlzyckntfqjonsie
+mwruvapdghplzyckwtfqjonsme
+mbruvapjghtlzyckwtfqgonsie
+pbruvapdghhlzyckwtfrjonsie
+mbruvgpdihxqzyckwtfqjonsie
+mbruvahdohxlzyckwtfijonsie
+ibuuvapdghxlzyckwtfqjofsie
+mbruvandghxlzyckwtfqjrnxie
+mbrjvlpdghxlzyckwgfqjonsie
+mbruvapogfxlzyckotfqjonsie
+mbruvrpdghxlzyckutfejonsie
+mbruvbpdghxlzyhkwtfqjonsip
+mbruvapdghxlzyckmnfqjensie
+mbruvapdghvlzyckwtfqjowsix
+mbruvakdgholzwckwtfqjonsie
+mbruvapdghxlzackwtfqconsae
+mbruvapdghxlzyqvwtfqjlnsie
+mprrvapdgfxlzyckwtfqjonsie
+mbrunacdghxlhyckwtfqjonsie
+obruvapdgsxlzyckwtfqjonvie
+murcvapdghslzyckwtfqjonsie
+mbruvapdghxlzyzkwmftjonsie
+mbrwvapdgtvlzyckwtfqjonsie
+mbxuvapdghxlzqcnwtfqjonsie
+mbruvaddghxboyckwtfqjonsie
+mhruvwndghxlzyckwtfqjonsie
+mbrdvapdghxlzyckwmpqjonsie
+mbruvapdgyxlzyckizfqjonsie
+mbruvapdghxlzlckwtfqeowsie
+mbruvbpdgrxlzyckwtfqjonsxe
+mbruqapoghxlzyckwtvqjonsie
+mbouhapdghmlzyckwtfqjonsie
+mbruvapjghxidyckwtfqjonsie
+mbsuvapkghxlkyckwtfqjonsie
+mbruvlpdghxlzycrwtfqjonsis
+mcrueapdghxlzyckwtfqjynsie
+muruvapngbxlzyckwtfqjonsie
+mbruvapdghxlzycawtfyjojsie
+mbruvbpdghxczyjkwtfqjonsie
+ybduvapdghxnzyckwtfqjonsie
+mbruvbpdghxlzyckwtfbjousie
+mbouvapdghxlzycbwtfqponsie
+mbruvaedghplzycgwtfqjonsie
+mbrhvapdghxlzyckytfqjgnsie
+mbruvapdqbxleyckwtfqjonsie
+mbruvapddhhldyckwtfqjonsie
+mbruvapdghxlwrckwtfqjondie
+mbruvapdmhxlzyckwtfqkonsve
+xbbuvapdghxlzyckwtfkjonsie
+mbruvapdghxlzyckwcfqjunkie
+mbruvapdghxlzyckwtfqxonfib
+mbrtvapkghxlzyckwtfqeonsie
+mbruvazdghxldymkwtfqjonsie
+kbruvapddhxlzfckwtfqjonsie
+mbouvapdghxlpyckwtfqjoosie
+mbauvapdghxlzyckwtfqjszsie
+mbruvapdghtlzyckntfqtonsie
+mbruvipdggxlzbckwtfqjonsie
+mbruqapdghrlzyckwtfqjznsie
+myruvacdghxlzyckwifqjonsie
+mbruvapdghxlzuckwtfkjocsie
+mwjuvapdghxlzyckwtfqjonsxe
+mbruvapxghxlzickwtfqjobsie
+mbrupapdghxtlyckwtfqjonsie
+meruvapdjjxlzyckwtfqjonsie
+mbruvkodghxlzyckwofqjonsie
+mbruvapdgexlzyckwtgkjonsie
+mbruvapwghxlzyckwtcqjonsiw
+mbruvapdghxlzykkwtfqtoxsie
+mbruvapdahxlzycgwtfwjonsie
+mbruvapdgwxlhyckhtfqjonsie
+mbruvapbghxlzycbhmfqjonsie
+mbruvapdghxvzyzkwtfqjonsin
+mbrcvapdqhxlzyckwyfqjonsie
+zbruvaxdghxlzyckwgfqjonsie
+mtruvapdghxlilckwtfqjonsie
+bbruvapdghxlzyckwtfmjonsxe
+mbruvajdghxlzyckwtfqfwnsie
+mbruvapdgkxlzyckwtfqionpie
+rbruvapdghxlryckwdfqjonsie
+mbruvandghxlzyckwmfvjonsie
+mbruvahdghxlzeckwtfqjonsme
+mbruvnpcghxlzyckwtfqjobsie
+mbruvapdghqlzyckwtfbjonsiy
+mbruvavdghxlzyckwufqjodsie
+mbruvapdghxlzyckwtfzmovsie
+mbruvlpdghxuzyckwtfqjoesie
+mbruvopdghxlzycwwtfqjansie
+obruvapdghglzybkwtfqjonsie
+mbpuvlpdghxlcyckwtfqjonsie
+mbruvaidghxlzyckwtfmjonoie
+mbruvapdihxzzyckwtfqjonsiy
+mbquvapdghxlzyckwtfqconsme
+mbruvapdghslzyckqtfqjojsie
+mbrzdapdghxmzyckwtfqjonsie
+mwruvapdghxozyckwtfqjonsxe
+muruvapdgfxlzyckwtfqjojsie
+wtruvapdghxlzyckvtfqjonsie
+mbruvapdghxlzyckysfqjxnsie
+mbruvrpdghxczyckktfqjonsie
+mbquvapdghxlryckwtfqjonsne
+mbruvapdghflzycvwtfqjpnsie
+mbruvapughclzyckwtfqjonsin
+mbrhvapdghxlpyckwtfqjonsre
+mbruvapdgtxlzyckwtfqjoosit
+mbrupapnghxhzyckwtfqjonsie
+mmvuvapdvhxlzyckwtfqjonsie
+mbruvaptghxlzyckwtfqjotsse
+mgruvapvghxlzyckwtfqjonsix
+mbrupapdghxszyckwtfqjunsie
+mbruvkpdghelzyckwtfqjpnsie
+mbruvrrdghjlzyckwtfqjonsie
+mbruvapdghnlzyckwtfkjonsze
+mbruvwpdghxlzyckwtfqhoysie
+mbrsvapdfhxlzyckwtfqjobsie
+mbruvapdgexezymkwtfqjonsie
+ybruvapdghxlzyckwtfqxonsiw
+mrruvapdghxdzyckwtfqjossie
+mbruvapdghtlzyckwtfqconsiu
+mbrpvapdghxlzlckwpfqjonsie
+mbruvjpdghslzyckwtfqjjnsie
+mhruvapoghxlzyckwtfvjonsie
+mbrubqpdghvlzyckwtfqjonsie
+mbruvapdghxlzackwtfqconsiw
+mbruvapdgnxlzkckwtfqjdnsie
+mbrudapgghelzyckwtfqjonsie
+mbruvapdghxlzlakwbfqjonsie
+mbpuvapdghxlzyckwtuqjonjie
+abruvapdghxlzykkwtfqjonzie
+mbrupupdghxlsyckwtfqjonsie
+mbrsvupdghxlzyckwtfqjonkie
+mxruvgpdghxllyckwtfqjonsie
+mbrnvapdghxlzycbwtfqfonsie
+mbrbxapdghxlzyckttfqjonsie
+mbnuvapdghxlzyxkwtmqjonsie
+mbrfvapdghjlzickwtfqjonsie
+mbhuvupdghxlzyxkwtfqjonsie
+mbrcvapdghxluyckwtfqjznsie
+mbruvapdghxlzyckwofqjoxsiz
+mbrevapdghxloyckwtfqjonnie
+mbruvipdghnlzyckwtfqjopsie
+mbxxvaptghxlzyckwtfqjonsie
+mbruvcpdghxlztckwtjqjonsie
+mqruvlpdghxlzyckotfqjonsie
+mbruvapdgqxlzyckwtfqjpvsie
+mbruvapdgvxlzyjkwtfqjbnsie
+mbruvapdghxlgyckwtfqcocsie
+mbruvapdghxkwyckwtfqjoqsie
+mbrgvavdghxlzyckwxfqjonsie
+qbruqapdgvxlzyckwtfqjonsie
+mbauvapdghxlzgckwtfqjunsie
+mbruvapdgdxluyckwtfqjoosie
+mbruvapdghxlzykkwtfqwobsie
+mbruvapdghxlzhcnwtfqjonqie
+mbruvapdghxlzycbhmfqjonsie
+mbruvapdghxluyczwtfqjontie
+mbruvapnghxlzyckwnfqjonbie
+moruvapdghxlzcckwtfqponsie
+mbruvapfgxxlzyckwtfqjunsie
+mbruvapdghxlryckvtfejonsie
+mbrzvapdghxlzvcbwtfqjonsie
+mbruvapdgqxlzyckwcfqjonsce
+abruvupdrhxlzyckwtfqjonsie
+mbrubaptghxlzyckwtfqjondie
+mgruvapdgpxlzyckwtfijonsie
+mbruvapdghxczlckwtfujonsie
+mbruvapdgmmlzyckwtfqjonsir
+mbruvapdhhxltyckwtfdjonsie
+mbruvapdghxlzyckwtfdjjnste
+mbrdvzpdghxlcyckwtfqjonsie
+mbruvapdghxlzyckwtnqbonsim
+mbrovapdghxlzyckwtfpjousie
+mymuvapdghxlzyjkwtfqjonsie
+mbpuvapdghxlzyckwtfljcnsie
+mbrxvapdghxlzyclwtfqjonpie
+mbrueapdghxlzyckwtfqjopsia
+mbruvapdghxlzycdwtfqjbfsie
+tbruvavdghxlzyckwtmqjonsie
+mbduvapdghxlzyckwrfqjrnsie
+mkrsvapughxlzyckwtfqjonsie
+mbruvapdghylzyckwtfqtolsie
+mgruvapdglxldyckwtfqjonsie
+mbrunapdghclzyckwtfqjonsiy
+mbruvapdgrxlxyckwtfgjonsie
+mbruvapdghxpzbckftfqjonsie
+mbruvcpdghxyzyckotfqjonsie
+mbruvapdghxlsyckwtfqcqnsie
+mbruvapdghxlzzckwtfqjonskf
+mbruvppdghxlzfckwtfqjgnsie
+mbhuvapdghxlzytkwtfqjonoie
+mbruvapdghxlzvrkwtfqjjnsie
+mbmuvapdghxuzyckwtfqjonsze
+mbruvapdghnlzycnwtfqjonsil
+mbruvapdgholzyckitfqjonsia
+mbruxapdghxlmyckwtfqbonsie
+mbauvapdgholzyckwtfqjolsie
+mbruvapdghxlzyckwtfqjotslq
+dbrutapdghxlzyckwtfqjonsiv
+mbruvapdzhxlyyckwtfbjonsie
+mmruaapsghxlzyckwtfqjonsie
+mbruvaldgqxqzyckwtfqjonsie
+mbruvaodghxdzyjkwtfqjonsie
+mbrcmatdghxlzyckwtfqjonsie
+mbrqvapdgtxlzycewtfqjonsie
+mjruvapdghzlzyckwtfqjonrie
+mbruvapdghxopcckwtfqjonsie
+mbruvapdghxszycwwtfqjoqsie
+mbruvapdgoxezyckwtjqjonsie';
+    $arr = explode("\n", $str);
+//    $arr = [
+//        'abcdef',
+//        'bababc',
+//        'abbcde',
+//        'abcccd',
+//        'aabcdd',
+//        'abcdee',
+//        'ababab',
+//    ];
+//    $arr = [
+//        'abcde',
+//        'fghij',
+//        'klmno',
+//        'pqrst',
+//        'fguij',
+//        'axcye',
+//        'wvxyz',
+//    ];
+
+    $total2 = 0;
+    $total3 = 0;
+    $arrAll = [];
+    foreach ($arr as $v) {
+        $arrTemp = str_split($v);
+
+        $arrCount = [];
+        foreach ($arrTemp as $char) {
+            if (array_key_exists($char, $arrCount)) {
+                $arrCount[$char] += 1;
+            } else {
+                $arrCount[$char] = 1;
+            }
+        }
+
+        if (array_search(2, $arrCount) !== false) {
+            $total2 += 1;
+        }
+        if (array_search(3, $arrCount) !== false) {
+            $total3 += 1;
+        }
+
+        $arrAll[] = $arrTemp;
+    }
+    $result = $total2 * $total3;
+    dump($result);
+
+    $arrClose = [];
+    for ($i = 0; $i < count($arrAll) - 1; $i++) {
+        for ($j = $i + 1; $j < count($arrAll); $j++) {
+            $arrDiff = [];
+            for ($k = 0; $k < count($arrAll[$i]); $k++) {
+                if ($arrAll[$i][$k] != $arrAll[$j][$k]) {
+                    $arrDiff[] = $k;
+                }
+            }
+            if (count($arrDiff) == 1) {
+                $temp = $arrAll[$i];
+                unset($temp[$arrDiff[0]]);
+                $arrClose[] = implode('', $temp);
+            }
+        }
+    }
+    dump($arrClose);
+?>
