cc9166f7d27f52900000fd8e8aa7d465934e72b0
Kiddo <jiaqun.l@mofadeng.com>
2020-12-02 13:57:58 +0800

    2020/2

--- 2020/Coder.php
+++ 2020/Coder.php
@@ -48,4 +48,59 @@ class Coder
             }
         }
     }
+
+    protected function toPasswordData()
+    {
+        $this->targetData = [];
+
+        foreach ($this->sourceData as $str) {
+            [$min, $max, $keyword, $temp, $code] = preg_split("/[:\s\-]/", $str);
+            $this->targetData[] = [
+                'min' => $min,
+                'max' => $max,
+                'keyword' => $keyword,
+                'code' => $code,
+            ];
+        }
+    }
+
+    public function getTotalPassword($part = 2)
+    {
+        $this->toPasswordData();
+        $list = $this->getPasswords($part);
+        return count($list);
+    }
+
+    protected function getPasswords(int $part)
+    {
+        $list = [];
+
+        foreach ($this->targetData as $rule) {
+            if ($part == 1) {
+                $isPassword = $this->checkKeyword1($rule['code'], $rule['keyword'], $rule['max'], $rule['min']);
+            } elseif ($part == 2) {
+                $isPassword = $this->checkKeyword2($rule['code'], $rule['keyword'], $rule['max'], $rule['min']);
+            } else {
+                $isPassword = false;
+            }
+            if ($isPassword) {
+                $list[] = $rule['code'];
+            }
+        }
+
+        return $list;
+    }
+
+    protected function checkKeyword1(string $code, string $keyword, int $max, int $min = 0)
+    {
+        $total = substr_count($code, $keyword);
+        return $min <= $total && $total <= $max;
+    }
+
+    protected function checkKeyword2(string $code, string $keyword, int $max, int $min = 0)
+    {
+        $min--;
+        $max--;
+        return $code[$min] == $keyword xor $code[$max] == $keyword;
+    }
 }
--- 2020/run.php
+++ 2020/run.php
@@ -2,11 +2,14 @@
 ini_set('memory_limit', '800M');
 require_once('./helper.php');
 
-$data = loadData(1);
+$data = loadData(2);
 
 $coder = new Coder($data);
 
-$part1 = $coder->getProduct(2); // 605364
-$part2 = $coder->getProduct(3); // 128397680
+//$part1 = $coder->getProduct(2); // 605364
+//$part2 = $coder->getProduct(3); // 128397680
+
+$part1 = $coder->getTotalPassword(1); // 477
+$part2 = $coder->getTotalPassword(2); // 686
 
 dd($part1, $part2);
